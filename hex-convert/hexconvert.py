c1d1193b259900aa595297cdb02d7a6035bbaf10
beegeiger <beegeiger@gmail.com>
2019-04-29 16:04:44 -0700

    Completed Hex convert

--- hex-convert/hexconvert.py
+++ hex-convert/hexconvert.py
@@ -16,6 +16,16 @@
 
 def hex_convert(hex_in):
     """Convert a hexadecimal string, like '1A', into it's decimal equivalent."""
+    hex_val = {"A": 10, "B": 11, "C": 12, "D": 13, "E": 14, "F": 15}
+    counter = 0
+    hex_in = hex_in[::-1]
+    for dig in range(0, len(hex_in)):
+        d = hex_in[dig]
+        if d.isdigit():
+            counter += int(d) * (16 ** dig)
+        else:
+            counter += hex_val[d] * (16 ** dig)
+    return counter
 
 
 if __name__ == "__main__":
